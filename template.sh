ca3555f01ca3f345ff729f1860e4cad51153f66c
Anil Prajapati <anilprajapti2002@gmail.com>
2026-04-22 02:05:52 +0530

    Project template created

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating Directories
+mkdir -p src
+mkdir -p research
+
+# Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trails.ipynb
+
+echo "Directories and files created successfully"
