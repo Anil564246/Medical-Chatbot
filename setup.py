349ca2c6690d1260307942ccb5745dbe6c1e8cc2
Anil Prajapati <anilprajapti2002@gmail.com>
2026-04-22 03:04:58 +0530

    Requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Anil Prajapati",
+    author_email="anilprajapti2002@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
